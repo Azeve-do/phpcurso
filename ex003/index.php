dc8029ef95b49164f1c9789ac66bd612af28ba18
Azeve-do <caiopaznunes@gmail.com>
2023-03-14 21:32:42 -0300

    variáveis php

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Why</h1>
+    <?php 
+
+        $nome = "Caio";
+        $sobrenome = "Paz";
+        const PAIS = "Brasil";
+
+        // PAIS = "EUA";
+
+        echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+    ?>
+</body>
+</html>
