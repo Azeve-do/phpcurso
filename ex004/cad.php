f6fd34b9e90cf64601399c4eb700aaed540674ae
Azeve-do <caiopaznunes@gmail.com>
2023-03-15 22:12:35 -0300

    adicionando infos a forms

---
+++ ex004/cad.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="estilophp.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+    <main>
+        <?php 
+           $Nome = $_GET["nome"] ?? "Sem nome";
+           $Sobrenome = $_GET["sobrenome"];
+
+           echo "<p>É um prazer te conhecer <strong>$Nome $Sobrenome</strong>! Este é o meu site!";
+        ?>
+        <a href="javascript:history.go(-1)">Voltar para a página anterior</a>
+    </main>
+</body>
+</html>
